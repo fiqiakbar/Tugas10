12b67b8f8ab969a0ded75b293ff3eab6174cceaa
Fiqi Akbar Trinanda <99949164+fiqiakbar@users.noreply.github.com>
2022-06-05 01:10:03 +0700

    Delete koneksi.inc.php

--- koneksi.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host='localhost';
-$username='root';
-$password='';
-$dbname='20082010162';
-$conn = mysqli_connect($host, $username, $password, $dbname) or die("Koneksi gagal dibangun");
-?>
